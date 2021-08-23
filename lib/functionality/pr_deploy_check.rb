ce792e36b32da9857188268f6d5b52622abaf069
Punit Jain <punit.jain@simplybusiness.co.uk>
2021-08-23 14:56:05 +0100

    emergency deploy

--- lib/functionality/pr_deploy_check.rb
+++ lib/functionality/pr_deploy_check.rb
@@ -8,7 +8,7 @@ require_relative 'base_deploy_check'
 class PrDeployCheck < BaseDeployCheck
   def self.check_labeled_pr(config)
     label_tags = SimplyIssue.get_label_tags(config)
-    if label_tags.include?('override_deploy')
+    if label_tags.include?('emergency deploy')
       result = config.client.create_status(config.app_repo, config.event_payload['pull_request']['head']['sha'],
                                            'success', description: 'You are free to deploy', context: context_name)
       puts "Created #{result[:state]} state with description #{result[:description]} for sha #{config.event_payload['pull_request']['head']['sha']} and url #{result[:url]}"
