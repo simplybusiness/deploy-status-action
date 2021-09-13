653e04ec5b0753479b5f3461b334201bdb6044f5
Punit Jain <punit.jain@simplybusiness.co.uk>
2021-09-13 11:02:25 +0100

    dont need to check on master

--- lib/functionality/base_deploy_check.rb
+++ lib/functionality/base_deploy_check.rb
@@ -8,7 +8,7 @@ require_relative 'simply_issue'
 class BaseDeployCheck
   def self.base_check(config, event, sha) # rubocop:disable Metrics/AbcSize
     puts "config event branch  #{config.event_branch}"
-    result = if !config.event_branch.include?('master') && SimplyIssue.get_all_issues(
+    result = if SimplyIssue.get_all_issues(
       config, event,
       'block deploys'
     ).length.positive?
