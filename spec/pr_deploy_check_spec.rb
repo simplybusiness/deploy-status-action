8e61df1044accdee2eb6cf029aef699179968c9b
Karan Shah <karan.shah@simplybusiness.co.uk>
2024-09-19 14:47:11 +0100

    SILVER-270 - Update test

--- spec/pr_deploy_check_spec.rb
+++ spec/pr_deploy_check_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe PrDeployCheck do
   context 'when pr is labeled with emergency deploy tag' do
     before do
       ENV['GITHUB_REPOSITORY'] = 'simplybusiness/important-app'
-      ENV['ISSUE_TOKEN'] = 'fake_token'
+      ENV['CLIENT_ID'] = 'client_id'
+      ENV['PRIVATE_KEY'] = JWT::JWK.new(OpenSSL::PKey::RSA.new(2048)).keypair.to_pem
       ENV['GITHUB_EVENT_PATH'] = Pathname.new(SPEC_FIXTURES_PATH).join('labeled_pr_payload.json').to_s
       ENV['GITHUB_REF'] = 'ref/my/base/branch'
       ENV['GITHUB_EVENT_NAME'] = 'pull_request'
