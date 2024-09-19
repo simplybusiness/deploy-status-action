413bf00446b932b148339dd4a49ecd1f2844f3b3
Karan Shah <karan.shah@simplybusiness.co.uk>
2024-09-19 11:47:11 +0100

    SILVER-270 - Remove extra lines

--- lib/config/github_api_config.rb
+++ lib/config/github_api_config.rb
@@ -9,18 +9,15 @@ require 'jwt'
 class GithubApiConfig
   def generate_jwt
     private_key = OpenSSL::PKey::RSA.new(ENV.fetch('PRIVATE_KEY'))
-
     payload = {
       iat: Time.now.to_i - 60,
       exp: Time.now.to_i + (10 * 60),
       iss: ENV.fetch('CLIENT_ID')
     }
-
     JWT.encode(payload, private_key, "RS256")
   end
 
   def initialize
-    generate_jwt
     @client = Octokit::Client.new(:bearer_token => generate_jwt)
     @app_repo = Octokit::Repository.new(ENV.fetch('GITHUB_REPOSITORY'))
     @event_payload = JSON.parse(File.read(ENV.fetch('GITHUB_EVENT_PATH')))
