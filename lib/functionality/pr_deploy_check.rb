be87d84d0208eb0c7d3beabae3e2ecb0ca624803
Jenah Blitz <jenah.blitz@simplybusiness.com>
2024-10-29 12:55:27 -0400

    Refactor file path in create_github_summary method

--- lib/functionality/pr_deploy_check.rb
+++ lib/functionality/pr_deploy_check.rb
@@ -18,12 +18,12 @@ class PrDeployCheck < BaseDeployCheck
       context: context_name,
       target_url: config.event_payload['html_url']
     )
-    github_summary_message = "Deploy Status Check\n"
+    github_summary_message = "## Deploy Status Check\n"
     github_summary_message += "### :sparkles: You are free to deploy :sparkles:\n" 
-    github_summary_message += " Created #{result[:state]} state with description #{result[:description]} for sha #{sha} and url #{result[:url]}\n"
-    github_summary_message += " description #{result[:description]} for sha #{sha} for url #{result[:url]}\n"
-    github_summary_message += " with target_url #{config.event_payload['html_url']}\n "
-    github_summary_message += ' ============================================================================================'
+    github_summary_message += "##### Created #{result[:state]} state with description #{result[:description]} for sha #{sha} and url #{result[:url]}\n"
+    github_summary_message += "##### description #{result[:description]} for sha #{sha} for url #{result[:url]}\n"
+    github_summary_message += "##### with target_url #{config.event_payload['html_url']}\n "
+    github_summary_message += '##### ============================================================================================'
     create_github_summary(github_summary_message)
     
     puts "Created #{result[:state]} state with" \
