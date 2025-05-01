f7a56c6021ea153ecd9bd89ea0e595ace6267f48
Robert Peacock <robert.peacock@simplybusiness.co.uk>
2025-05-01 16:21:19 +0100

    fix Metrics cops in SimplyIssue

--- lib/functionality/simply_issue.rb
+++ lib/functionality/simply_issue.rb
@@ -6,12 +6,15 @@ require 'octokit'
 # Fetch the labels on PR and issue
 class SimplyIssue
   def self.get_label_tags(config)
-    if ['pull_request', 'pull_request_target'].include?(config.event_name)
-      puts "Pull request #{config.event_payload['number']}"
-      issue = config.client.pull_request(config.app_repo, config.event_payload['number'])
-    else
-      issue = config.client.issue(config.app_repo, config.event_payload['issue']['number'])
-    end
+    repo = config.app_repo
+    payload = config.event_payload
+    client = config.client
+
+    issue = if is_pr_event?(config)
+              client.pull_request(repo, payload['number'])
+            else
+              client.issue(repo, payload['issue']['number'])
+            end
 
     issue['labels'].inject([]) { |memo, label| memo.push(label['name']) }
   end
@@ -28,5 +31,12 @@ class SimplyIssue
   def self.block_deploys?(config, event)
     any_block_deploys = get_all_issues(config, event, 'block deploys').length.positive?
     any_block_deploys && !SimplyIssue.get_label_tags(config).include?('emergency deploy')
+  end
+
+  def self.is_pr_event?(config)
+    is_pr_event = ['pull_request', 'pull_request_target'].include?(config.event_name)
+    puts "Pull request #{config.event_payload['number']}" if is_pr_event
+
+    is_pr_event
   end
 end
