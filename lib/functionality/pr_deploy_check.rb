dc0f3033952f8e4279c675f921625bede9fd48a0
Robert Peacock <robert.peacock@simplybusiness.co.uk>
2025-05-01 11:18:23 +0100

    fix Layout/LineLength

--- lib/functionality/pr_deploy_check.rb
+++ lib/functionality/pr_deploy_check.rb
@@ -18,12 +18,14 @@ class PrDeployCheck < BaseDeployCheck
       context: context_name,
       target_url: config.event_payload['html_url']
     )
-    github_summary_message = "## Deploy Status Check!\n"
-    github_summary_message += "### :sparkles: You are free to deploy :sparkles:\n"
-    github_summary_message += "- Created #{result[:state]} state with description #{result[:description]} for sha #{sha} and url #{result[:url]}\n"
-    github_summary_message += "- description #{result[:description]} for sha #{sha} for url #{result[:url]}\n"
-    github_summary_message += "- with target_url #{config.event_payload['html_url']}\n "
-    create_github_summary(github_summary_message)
+    message = <<~MESSAGE
+      ## Deploy Status Check!
+      ### :sparkles: You are free to deploy :sparkles:
+      - Created #{result[:state]} state with description #{result[:description]} for sha #{sha} and url #{result[:url]}
+      - Description: #{result[:description]} for sha #{sha} for url #{result[:url]}
+      - With target_url: #{config.event_payload['html_url']}
+    MESSAGE
+    create_github_summary(message)
 
     puts "Created #{result[:state]} state with" \
          "description #{result[:description]} for sha #{sha} for url #{result[:url]} " \
